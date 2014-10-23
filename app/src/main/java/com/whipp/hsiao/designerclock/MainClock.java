bdf165343f5a5cdef17d875ad1ca854982d47b65
awhipp <awhipp@terpmail.umd.edu>
2014-10-23 16:32:57 -0400

    Initial commit

---
+++ app/src/main/java/com/whipp/hsiao/designerclock/MainClock.java
@@ -0,0 +1,71 @@
+package com.whipp.hsiao.designerclock;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainClock extends Activity {
+
+    private TextView clockText;
+    private WatchViewStub stub;
+    private SharedPreferences prefs;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_clock);
+        stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            public void onLayoutInflated(WatchViewStub stub) {
+                clockText = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+        new UpdateClock().execute(this);
+
+    }
+
+    private class UpdateClock extends AsyncTask<Context, Integer, Boolean> {
+
+        protected Boolean doInBackground(Context... context) {
+            clockText = (TextView) stub.findViewById(R.id.text);
+            new Timer().scheduleAtFixedRate(new TimerTask(){
+                public void run() {
+                    runOnUiThread(new Runnable(){
+                        public void run() {
+                            Calendar time;
+                            time = Calendar.getInstance();
+                            int hour;
+                            String mode = prefs.getString("mode","12");
+                            if(mode.equals("12")){
+                                hour = time.get(Calendar.HOUR);
+                            }else{
+                                hour = time.get(Calendar.HOUR_OF_DAY);
+
+                            }
+                            int minute = time.get(Calendar.MINUTE);
+                            int sec = time.get(Calendar.SECOND);
+                            clockText.setText(hour + ":" + String.format("%02d", minute) + ":" + String.format("%02d", sec));
+                            if(mode.equals("12"))
+                                if(time.get(Calendar.HOUR_OF_DAY) < 12){
+                                    clockText.append(" AM");
+                                }else{
+                                    clockText.append(" PM");
+                                }
+                        }});
+                }
+            }, 0, 500);
+
+            return true;
+        }
+    }
+}
